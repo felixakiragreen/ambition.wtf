8bb1cc982b28e5cddff8c8e83160f6645ce1310a
Felix Green <work@felix.green>
2021-03-25 19:32:45 -0400

    add heading

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,16 +2,15 @@ import React from 'react'
 import { styled } from '../ui'
 
 import { Content, Box, Img } from './Box'
-import { Text, Link } from './Text'
+import { Text, Link, UnderlinedHeading, ExtraHeading } from './Text'
 import { HexIcon } from './Hexagon'
 
 import { socials } from '../content/SocialLinks'
 
-import logo from '../assets/ambition_sharp_flat_border.svg'
+import logo from '../assets/ambition_round_gradient_border.svg'
 
 export const StyledFooter = styled('footer', {
-  background: '$background',
-  color: '$foreground',
+  background: '$mirage',
 })
 
 export const Footer: React.FC = ({ children }) => {
@@ -19,10 +18,15 @@ export const Footer: React.FC = ({ children }) => {
     <StyledFooter>
       <Content>
         <Box align="centerX" justify="spaceBetweenX">
-          <Box align="centerX" spacing="x">
-            {socials.map((social) => (
-              <HexIcon key={social.title} {...social} />
-            ))}
+          <Box>
+            <UnderlinedHeading id="where-the-fuck">
+              Where the fuck<ExtraHeading> (Socials)</ExtraHeading>
+            </UnderlinedHeading>
+            <Box align="centerX" spacing="x">
+              {socials.map((social) => (
+                <HexIcon key={social.title} {...social} />
+              ))}
+            </Box>
           </Box>
           <Box>
             <Link href="http://ambition.wtf" target="_blank">
