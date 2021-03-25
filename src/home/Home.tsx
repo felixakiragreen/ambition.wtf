deb820c3c65efe917406f739011c4366120dec20
Felix Green <work@felix.green>
2021-03-25 16:42:12 -0400

    clean up, remove

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -1,45 +1,32 @@
 import React from 'react'
 
 import { styled, darkTheme } from '../ui'
-import { Page, Content, Box, SvgPixels } from '../components'
-import { AccountsList } from '../types'
+import { Page, Content } from '../components'
 
 import Hero from './Hero.mdx'
-import Buy from './Buy'
 import Faq from './Faq.mdx'
 import Behind from './Behind.mdx'
 
-type Props = {
-  accounts: AccountsList
-  updateAccounts: (accounts: AccountsList) => void
-}
+
 
 export const StyledAside = styled('aside', {
   background: '$background',
   color: '$foreground',
 })
 
-export const Home: React.FC<Props> = ({ accounts, updateAccounts }) => {
+type Props = {
+}
+
+export const Home: React.FC<Props> = ({}) => {
   return (
     <Page>
       <Content>
         <Hero />
       </Content>
-
-      <Buy accounts={accounts} updateAccounts={updateAccounts} />
       <Content>
         <Faq />
       </Content>
       <StyledAside className={darkTheme}>
-        <Box
-          css={{
-            display: 'flex',
-            background: '$white',
-            color: '$background',
-          }}
-        >
-          <SvgPixels />
-        </Box>
         <Content>
           <Behind />
         </Content>
