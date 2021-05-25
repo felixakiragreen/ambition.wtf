b7909156aa2ed91b28766c811748ebc3dd0f7e4c
Felix Green <work@felix.green>
2021-05-25 19:54:58 -0400

    use the new hex icon component

--- src/lib/comps/Socials.svelte
+++ src/lib/comps/Socials.svelte
@@ -1,49 +1,11 @@
 <script lang="ts">
 	import { socials } from '@/lib/datum/socials'
-
-	import HexIcon from '../bonds/HexIcon.svelte'
-	import Inline from '../bonds/Inline.svelte'
-
-	import { map } from 'lodash'
-	import type { SocialLink } from '@/types'
-
-	let listSocials: SocialLink[] = map(
-		socials,
-		(value: SocialLink, key, collection) => value
-	)
-
-	// console.log({ listSocials })
-
-	// const ss = stitch({
-	// 	display: 'grid',
-	// 	gridTemplateColumns: 'repeat(auto-fill, $16)',
-	// 	w: '$96',
-
-	// 	'& a': {
-	// 		// size: '$16',
-	// 	},
-	// })
+	import HexIconRow from './HexIconRow.svelte'
 
 	export let space = 'sm'
 	export let align = null
 	export let size = 'md'
 	export let css = null
-
-	const iconSizes = {
-		sm: '$8',
-		md: '$12',
-		lg: '$16',
-		xl: '$20',
-		'2xl': '$24',
-	}
-
-	$: iconSize = iconSizes[size] || size
-
-	$: cssInl = css || { '> *': { size: iconSize } }
 </script>
 
-<Inline css={cssInl} {space} {align}>
-	{#each listSocials as social}
-		<HexIcon {...social} />
-	{/each}
-</Inline>
+<HexIconRow {socials} {css} {space} {align} {size} look={'brand'} />
