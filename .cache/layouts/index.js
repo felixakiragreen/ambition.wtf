709b631a6d7166a63ae2cf370fe5377b8e44ec5d
Felix Green <work@felix.green>
2021-05-25 18:26:57 -0400

    delete cache shit

--- .cache/layouts/index.js
+++
@@ -1,7 +0,0 @@
-
-  import React from "react"
-  import Component from "/Users/luke/dev/git/ambition.wtf/src/layouts/index.js"
-  import data from "/Users/luke/dev/git/ambition.wtf/.cache/json/layout-index.json"
-
-  export default (props) => <Component {...props} {...data} />
-  
