2c4b347abe71aa7872589143781711bd3ac2794a
Felix Green <work@felix.green>
2021-05-25 18:30:05 -0400

    types

---
+++ src/types.ts
@@ -0,0 +1,17 @@
+export type SocialLink = {
+	url: string
+	username: string
+	title: string
+	hex: string
+	path: string
+	transform: {
+		scale: number
+		translate: {
+			x: number
+			y: number
+		}
+	}
+	color?: string
+	background?: string
+	foreground?: string
+}
