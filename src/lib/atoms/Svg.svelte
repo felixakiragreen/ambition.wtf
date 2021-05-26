a8d48f1850ddb993bd1ed0e2910670e76b8a18ad
Felix Green <work@felix.green>
2021-05-26 13:25:23 -0400

    temp remove props

--- src/lib/atoms/Svg.svelte
+++ src/lib/atoms/Svg.svelte
@@ -1,19 +1,15 @@
 <script>
 	export let viewBox
+
+	// TEMP: removed
+	// fill-rule="evenodd"
+	// clip-rule="evenodd"
+	// stroke-line-join="round"
+	// stroke-linecap="round"
+	// stroke-linejoin="round"
+	// stroke-miter-limit={2}
 </script>
 
-<svg
-	{viewBox}
-	x="0%"
-	y="0%"
-	width="100%"
-	height="100%"
-	fill-rule="evenodd"
-	clip-rule="evenodd"
-	stroke-line-join="round"
-	stroke-linecap="round"
-	stroke-linejoin="round"
-	stroke-miter-limit={2}
->
+<svg {viewBox} x="0%" y="0%" width="100%" height="100%">
 	<slot />
 </svg>
