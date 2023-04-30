895618b33f31402a7fc0c3399fe1554007a174d1
Felix Green <work@felix.green>
2023-04-30 19:51:38 -0400

    Create +page.svelte

---
+++ src/routes/wawegonado/privacy/+page.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { stitch } from '@/ui'
+
+	import { Box, Image } from '@/lib/atoms'
+	import { Content, Text } from '@/lib/bonds'
+
+	import { HeadingExtra, HeadingUnderline } from '@/lib/cells'
+
+	import Privacy from '../privacy.svelte'
+
+	const ss = stitch({
+		bg: '$background',
+		color: '$foreground',
+		fontFamily: '$mono',
+	})
+</script>
+
+<Box cls={ss}>
+	<Content>
+		<HeadingUnderline id="privacy">
+			Privacy <HeadingExtra content="(Policy)" />
+		</HeadingUnderline>
+
+		<Privacy />
+	</Content>
+</Box>
