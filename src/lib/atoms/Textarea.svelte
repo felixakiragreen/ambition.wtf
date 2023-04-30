a5c16e14ba283e4802659d0ba292cb60da4a3e73
Felix Green <work@felix.green>
2023-04-30 19:50:51 -0400

    Create Textarea.svelte

---
+++ src/lib/atoms/Textarea.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import { stitch } from '@/ui'
+	import type { CSS } from '@/ui'
+
+	export let id = null
+	export let cls = null
+	export let css: CSS = null
+	export let style = null
+	export let vrt = {}
+
+	export let value = ''
+	export let placeholder = ''
+	export let lines = null
+	let rows = lines !== null ? lines : undefined
+
+	const ss = cls || stitch({})
+</script>
+
+<textarea
+	class={ss({ ...vrt, css })}
+	{style}
+	{id}
+	bind:value
+	on:input
+	on:change
+	on:blur
+	{placeholder}
+	{rows}
+/>
