8f86fb24e545a0b29f43330cf409a50531695ae3
Felix Green <work@felix.green>
2023-04-30 18:25:40 -0400

    add privacy policy

---
+++ src/lib/comps/PrivacyWawegonado.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+	import Text from '@/lib/bonds/Text.svelte'
+	import Stack from '@/lib/bonds/Stack.svelte'
+</script>
+
+<Stack>
+	<Text
+		>We respect your privacy and are committed to protecting your personal
+		information. This Privacy Policy explains how we collect, use, and disclose
+		information through our app, "WaWeGonaDo‽"
+
+		<br /><br /><Text as="h2">Information We Collect</Text>
+		We do not collect any personal information from users of our app. We do not track,
+		store, or share any personal data, including but not limited to names, email
+		addresses, phone numbers, or any other identifying information.
+
+		<br /><br /><Text as="h2">Use of Information</Text>
+		As we do not collect any personal information, we do not use any information
+		for any purpose.
+
+		<br /><br /><Text as="h2">Sharing of Information</Text>
+		As we do not collect any personal information, we do not share any information
+		with third parties. However, polls and votes created in the app are stored in
+		the iCloud public database, which is a first-party database controlled by Apple.
+		No information is stored or collected anywhere else.
+
+		<br /><br /><Text as="h2">Data Security</Text>
+		We take data security seriously and use industry-standard measures to protect
+		the information you provide to us. However, as we do not collect any personal
+		information, there is no data to be protected or secured.
+
+		<br /><br /><Text as="h2">Contact Us</Text>
+		If you have any questions or concerns about this Privacy Policy, please contact
+		us using the address: email at ambition.wtf.
+
+		<br /><br /><Text as="h2">Updates to Privacy Policy</Text>
+		We may update this Privacy Policy from time to time. Any changes we make will
+		be posted on this page. We encourage you to review this Privacy Policy periodically
+		to stay informed about our data protection practices.<br /><br />
+
+		Thank you for using WaWeGonaDo‽
+	</Text>
+</Stack>
