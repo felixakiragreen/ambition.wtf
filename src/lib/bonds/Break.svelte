e7ddedaf4608361ed867099779e6c4a5039ba943
Felix Green <work@felix.green>
2023-04-30 19:50:52 -0400

    Create Break.svelte

---
+++ src/lib/bonds/Break.svelte
@@ -0,0 +1,58 @@
+<script lang="ts">
+	import { stitch } from '@/ui'
+	import type { CSS } from '@/ui'
+
+	import { Span } from '@/lib/atoms'
+
+	// as text or as box
+
+	// this component handles all the break points shit for text...
+
+	// export let as = 'span'
+
+	export let css: CSS = null
+	export let all = null
+	export let no = null
+	export let sm = null
+	export let md = null
+
+	const ss = stitch({
+		variants: {
+			all: {
+				// breakpoints break
+				true: {
+					wordBreak: 'break-all',
+				},
+			},
+			no: {
+				true: {
+					whiteSpace: 'nowrap',
+				},
+			},
+			sm: {
+				true: {
+					//
+					'@md': {
+						display: 'none',
+					},
+				},
+			},
+			md: {
+				true: {
+					display: 'none',
+					'@md': {
+						display: 'initial',
+					},
+				},
+			},
+		},
+	})
+</script>
+
+<Span cls={ss} vrt={{ all, no, sm, md }} {css}>
+	{#if $$slots.default}
+		<slot />
+	{:else}
+		<br />
+	{/if}
+</Span>
