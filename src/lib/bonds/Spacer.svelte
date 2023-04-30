124be9e748cb822b10a51377b3872dda50ff469d
Felix Green <work@felix.green>
2023-04-30 19:51:05 -0400

    Create Spacer.svelte

---
+++ src/lib/bonds/Spacer.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+	import { stitch } from '@/ui'
+	import type { CSS } from '@/ui'
+
+	import { Box } from '@/lib/atoms'
+
+	export let id = null
+	export let css: CSS = null
+
+	type Axis = 'x' | 'y'
+
+	export let axis: Axis = 'x'
+	export let color = 'muted'
+
+	const ss = stitch({
+		variants: {
+			axis: {
+				x: {
+					h: '$px',
+					w: '$full',
+				},
+				y: {
+					// h: '$full',
+					w: '$px',
+				},
+			},
+			color: {
+				dusky: {
+					bg: '$dusky',
+				},
+				muted: {
+					bg: '$muted',
+				},
+				jaded: {
+					bg: '$jaded',
+				},
+				faded: {
+					bg: '$faded',
+				},
+				faint: {
+					bg: '$faint',
+				},
+				clear: {
+					bg: '$clear',
+				},
+			},
+		},
+	})
+</script>
+
+<Box cls={ss} vrt={{ axis, color }} {css} {id}>
+	<slot />
+</Box>
