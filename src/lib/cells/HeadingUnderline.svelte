87af96ae86f797296dde37a9adc84b304b631280
Felix Green <work@felix.green>
2023-04-30 19:51:10 -0400

    Create HeadingUnderline.svelte

---
+++ src/lib/cells/HeadingUnderline.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import Text from '../bonds/Text.svelte'
+	import Link from '../bonds/Link.svelte'
+
+	export let id
+</script>
+
+<Link url={`#${id}`} look="clean">
+	<Text
+		as="h3"
+		mdx="h5"
+		look="mono"
+		{id}
+		css={{
+			textTransform: 'uppercase',
+			color: '$foreground',
+			borderBottom: '2px solid $felixgreen',
+			pointerEvents: 'none', // necessary to not cover any shit up
+			'@initial': {
+				pb: '$2',
+			},
+			'@md': {
+				pb: '$4',
+			},
+			scrollMarginTop: '$40',
+		}}
+	>
+		<slot />
+	</Text>
+</Link>
