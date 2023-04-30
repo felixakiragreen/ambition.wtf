bc49b0b933851c3445c8572625efb0bcd6520cf2
Felix Green <work@felix.green>
2023-04-30 19:50:47 -0400

    Update Image.svelte

--- src/lib/atoms/Image.svelte
+++ src/lib/atoms/Image.svelte
@@ -1,14 +1,24 @@
-<script>
+<script lang="ts">
 	import { stitch } from '@/ui'
+	import type { CSS } from '@/ui'
 
+	export let id = null
 	export let cls = null
-	export let css = null
+	export let css: CSS = null
+	export let style = null
+
 	export let alt = 'image description'
 	export let url
-	export let style = null
-	export let id = null
+	export let url2x = null
 
 	const ss = cls || stitch({})
 </script>
 
-<img class={ss({ css })} {alt} src={url} {style} {id} />
+<img
+	class={ss({ css })}
+	{alt}
+	src={url}
+	{...url2x ? { srcset: `${url2x} 2x` } : {}}
+	{style}
+	{id}
+/>
