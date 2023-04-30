262180c0a509ac2b09633c3e5c3eb5df19b7175e
Felix Green <work@felix.green>
2023-04-30 19:51:32 -0400

    Create +layout.svelte

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	import '@/lib/style/global.css'
+	import { stitch, getCssText } from '@/ui'
+
+	const ss = stitch({
+		display: 'grid',
+		gridTemplateRows: 'auto 1fr auto',
+		minHeight: '100vh',
+		position: 'relative',
+	})
+
+	import Footer from './footer.svelte'
+</script>
+
+<svelte:head>
+	<title>ambition</title>
+	<script
+		src="https://cdn.usefathom.com/script.js"
+		data-site="VMNRMHGP"
+		data-excluded-domains="localhost"
+		defer
+	>
+	</script>
+</svelte:head>
+
+<!-- https://discord.com/channels/752614004387610674/752795957674115092/925177257025220658 -->
+<!-- https://stackblitz.com/edit/sveltekit-1sfugv?file=src/routes/__layout.svelte -->
+<!-- for TS projects due to a bug (https://github.com/sveltejs/svelte/issues/5292)-->
+{@html `<${''}style id="stitches">${getCssText()}</${''}style>`}
+
+<main class={ss()}>
+	<div>
+		<slot />
+	</div>
+
+	<Footer />
+</main>
