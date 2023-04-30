bd9faa82de08cedab570d2d03d5944c7123f8ee6
Felix Green <work@felix.green>
2023-04-30 19:51:10 -0400

    Create index.ts

---
+++ src/lib/cells/index.ts
@@ -0,0 +1,7 @@
+export { default as About } from './About.svelte'
+export { default as HeadingExtra } from './HeadingExtra.svelte'
+export { default as HeadingLink } from './HeadingLink.svelte'
+export { default as HeadingUnderline } from './HeadingUnderline.svelte'
+export { default as Projects } from './Projects.svelte'
+export { default as Socials } from './Socials.svelte'
+export { default as Team } from './Team.svelte'
