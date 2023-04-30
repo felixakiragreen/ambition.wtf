3eb97f40aad3b3d3b21098199aab43e9505c190a
Felix Green <work@felix.green>
2023-04-30 19:50:46 -0400

    Update Anchor.svelte

--- src/lib/atoms/Anchor.svelte
+++ src/lib/atoms/Anchor.svelte
@@ -1,19 +1,30 @@
 <script lang="ts">
 	import { stitch } from '@/ui'
+	import type { CSS } from '@/ui'
+
+	export let id = null
+	export let cls = null
+	export let css: CSS = null
+	export let style = null
+	export let vrt = {}
 
 	export let url
+	export let fileName = undefined
+	let download = fileName
 	export let newTab = false
 	let target = newTab ? '_blank' : '_self'
-
-	export let cls = null
-	export let css = null
-	export let style = null
-	export let id = null
-	export let vrt = {}
 
 	const ss = cls || stitch({})
 </script>
 
-<a href={url} {target} class={ss({ ...vrt, css })} {style} {id}>
+<a
+	href={url}
+	{download}
+	{target}
+	class={ss({ ...vrt, css })}
+	{style}
+	{id}
+	on:click
+>
 	<slot />
 </a>
