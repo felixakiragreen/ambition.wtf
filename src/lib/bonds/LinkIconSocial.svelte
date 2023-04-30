d8e75b25c4a9a9913b2f9595cbacb316360f5c8b
Felix Green <work@felix.green>
2023-04-30 19:51:02 -0400

    Create LinkIconSocial.svelte

---
+++ src/lib/bonds/LinkIconSocial.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import Link from './Link.svelte'
+	import IconSocial from './IconSocial.svelte'
+
+	export let url
+	export let newTab = true
+	export let hex
+	export let hue
+	export let path
+	export let transform
+
+	// variants
+	export let symbol = null
+	export let ground = null
+	export let hoverSymbol = null
+	export let hoverGround = null
+</script>
+
+<Link {url} {newTab} look="clean">
+	<IconSocial
+		brandHue={hue}
+		brandExact={hex}
+		{...{ symbol, ground, hoverSymbol, hoverGround }}
+		{path}
+		{transform}
+	/>
+</Link>
