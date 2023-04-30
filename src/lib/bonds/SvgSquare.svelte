0100976e090d77146a2eea9b5ce0e8baa2ba0321
Felix Green <work@felix.green>
2023-04-30 19:51:07 -0400

    Create SvgSquare.svelte

---
+++ src/lib/bonds/SvgSquare.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { Svg } from '@/lib/atoms'
+
+	export let viewBox = '4 4 28 28'
+	export let fill = 'none'
+
+	// export let rounded = false
+</script>
+
+<Svg {viewBox}>
+	<path {fill} d="M0 0H36V36H0z" />
+
+	<slot />
+</Svg>
