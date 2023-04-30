c883abbf8937649c58af7d6fc9139a11e1265159
Felix Green <work@felix.green>
2023-04-30 19:51:09 -0400

    Create HeadingLink.svelte

---
+++ src/lib/cells/HeadingLink.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+	import { stitch } from '@/ui'
+	import type { CSS } from '@/ui'
+
+	import Link from '../bonds/Link.svelte'
+	import Text from '../bonds/Text.svelte'
+
+	export let url: string
+	export let newTab = false
+
+	export let css: CSS = {}
+</script>
+
+<Text look="mono">
+	<Link
+		look="blocky"
+		css={{
+			...css,
+			textTransform: 'uppercase',
+			'&:hover': {
+				px: '$2',
+				mx: '$-2',
+				color: '$indigo',
+				background: '$felixgreen',
+			},
+			'&:after': {
+				'@initial': {
+					content: '',
+				},
+				'@md': {
+					content: ' tf',
+				},
+				'@lg': {
+					content: ' the fuck',
+				},
+			},
+		}}
+		{url}
+		{newTab}
+	>
+		<slot />
+	</Link>
+</Text>
