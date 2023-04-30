58a242cdf176943c96c48f5c38e8acea1ee46bc0
Felix Green <work@felix.green>
2023-04-30 19:51:39 -0400

    Update ui.ts

--- src/ui.ts
+++ src/ui.ts
@@ -1,3 +1,5 @@
-import { css, keyframes, getCssString, globalCss } from '../stitches.config'
+import { css, keyframes, getCssText, globalCss } from './stitches.config'
+import type { CSS } from './stitches.config'
 
-export { css as stitch, keyframes, getCssString, globalCss }
+export { css as stitch, keyframes, getCssText, globalCss }
+export type { CSS }
