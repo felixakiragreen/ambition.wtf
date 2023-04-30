445044529c5b35ef55752e396b4627d718757460
Felix Green <work@felix.green>
2023-04-30 19:51:13 -0400

    Create Socials.svelte

---
+++ src/lib/cells/Socials.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { socials } from '@/lib/datum/socials'
+	import IconSocialRow from '../bonds/IconSocialRow.svelte'
+
+	export let space = 'sm'
+	export let align = null
+	export let size = 'md'
+	export let css = null
+
+	export let filter: string[] = null
+
+	// variants
+	export let symbol = null
+	export let ground = null
+	export let hoverSymbol = null
+	export let hoverGround = null
+</script>
+
+<IconSocialRow
+	{socials}
+	{css}
+	{space}
+	{align}
+	{size}
+	{...{ symbol, ground, hoverSymbol, hoverGround }}
+/>
