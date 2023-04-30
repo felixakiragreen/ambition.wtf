54c22b2048d9308ae415108ba5f328d2967f3d79
Felix Green <work@felix.green>
2023-04-30 19:50:48 -0400

    Create Input.svelte

---
+++ src/lib/atoms/Input.svelte
@@ -0,0 +1,98 @@
+<script lang="ts">
+	import { stitch } from '@/ui'
+	import type { CSS } from '@/ui'
+
+	export let id = null
+	export let cls = null
+	export let css: CSS = null
+	export let style = null
+	export let vrt = {}
+
+	type InputTypes = `password` | `email` | `text` | `number` | `date` | `time`
+
+	export let inputType: InputTypes = 'text'
+	export let value = ''
+	export let placeholder = ''
+
+	const ss = cls || stitch({})
+</script>
+
+{#if inputType === 'password'}
+	<input
+		type="password"
+		class={ss({ ...vrt, css })}
+		{style}
+		{id}
+		bind:value
+		on:input
+		on:change
+		on:focus
+		on:blur
+		{placeholder}
+	/>
+{:else if inputType === 'email'}
+	<input
+		type="email"
+		class={ss({ ...vrt, css })}
+		{style}
+		{id}
+		bind:value
+		on:input
+		on:change
+		on:focus
+		on:blur
+		{placeholder}
+	/>
+{:else if inputType === 'number'}
+	<input
+		type="number"
+		class={ss({ ...vrt, css })}
+		{style}
+		{id}
+		bind:value
+		on:input
+		on:change
+		on:focus
+		on:blur
+		{placeholder}
+	/>
+{:else if inputType === 'date'}
+	<input
+		type="date"
+		class={ss({ ...vrt, css })}
+		{style}
+		{id}
+		bind:value
+		on:input
+		on:change
+		on:focus
+		on:blur
+		{placeholder}
+	/>
+{:else if inputType === 'time'}
+	<input
+		type="time"
+		class={ss({ ...vrt, css })}
+		{style}
+		{id}
+		bind:value
+		on:input
+		on:change
+		on:focus
+		on:blur
+		{placeholder}
+	/>
+{:else}
+	<input
+		type="text"
+		class={ss({ ...vrt, css })}
+		{style}
+		{id}
+		bind:value
+		on:input
+		on:change
+		on:focus
+		on:blur
+		{placeholder}
+	/>
+{/if}
