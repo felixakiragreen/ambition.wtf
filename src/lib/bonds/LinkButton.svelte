72c6feb09befaec2d47dfedb82bde216012778c2
Felix Green <work@felix.green>
2023-04-30 19:51:02 -0400

    Create LinkButton.svelte

---
+++ src/lib/bonds/LinkButton.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import Link from './Link.svelte'
+	import Button from './Button.svelte'
+
+	export let url
+	export let fileName = undefined
+	export let newTab = false
+
+	// export let css = null
+	// export let look = 'main'
+	// export let size = 'md'
+	// export let rounding = 'none'
+	// export let disabled = null
+</script>
+
+<Link {url} {fileName} {newTab} on:click look="clean">
+	<Button {...$$props}>
+		<slot />
+	</Button>
+</Link>
