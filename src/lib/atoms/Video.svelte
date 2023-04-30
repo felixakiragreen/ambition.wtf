4d2ba50e574f25026b597697a5e865c35d3f2a1d
Felix Green <work@felix.green>
2023-04-30 19:50:52 -0400

    Create Video.svelte

---
+++ src/lib/atoms/Video.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { stitch } from '@/ui'
+	import type { CSS } from '@/ui'
+
+	export let id = null
+	export let cls = null
+	export let css: CSS = null
+	export let style = null
+
+	export let url
+	export let loop = true
+	export let autoPlay = true
+	export let type = 'video/mp4'
+
+	const ss = cls || stitch({})
+</script>
+
+<video class={ss({ css })} {style} {id} {loop} autoplay={autoPlay}>
+	<source src={url} {type} />
+	<track default kind="captions" />
+</video>
